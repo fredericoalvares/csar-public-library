50cdf47f21f70c4f3f55fd23cb84af0218ebbd0e
Frederico Alvares <fredericoalvares@gmail.com>
2019-03-20 22:37:46 +0100

    fix influx install script

--- com/easyvirt/influxdb/linux/scripts/install_influxdb.sh
+++ com/easyvirt/influxdb/linux/scripts/install_influxdb.sh
@@ -5,7 +5,7 @@ DISTRIB_CODENAME=$(cat /etc/*-release | sed -n 's/^VERSION=.*\s(\(.*\))"$/\1/p')
 NAME="InfluxDB"
 LOCK="/tmp/lockaptget"
 
-echo "Using apt-get. Installing influxdb on one of the following : ${DISTRIB_ID} / ${DISTRIB_CODENAME}"
+#echo "Using apt-get. Installing influxdb on one of the following : ${DISTRIB_ID} / ${DISTRIB_CODENAME}"
 
 #while true; do
 #  if mkdir "${LOCK}" &>/dev/null; then
@@ -21,14 +21,20 @@ echo "Using apt-get. Installing influxdb on one of the following : ${DISTRIB_ID}
 #  sleep 0.5
 #done
 #
-wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
-source /etc/lsb-release
-echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+#wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+#source /etc/lsb-release
+#echo "deb https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
+
+
+
 #
 #
 #sudo rm -f /var/lib/dpkg/lock
-sudo apt-get update || (sleep 15; sudo apt-get update || exit ${1})
-sudo apt-get install -y -q influxdb || exit ${1}
+#sudo apt-get update || (sleep 15; sudo apt-get update || exit ${1})
+#sudo apt-get install -y -q influxdb || exit ${1}
+wget https://dl.influxdata.com/influxdb/releases/influxdb_1.7.4_amd64.deb
+sudo dpkg -i influxdb_1.7.4_amd64.deb
+
 sudo systemctl unmask influxdb.service
 
 #rm -rf "${LOCK}"
