c1d1545e24d7998884ffcf52c2fccd961d649f73
Frederico Alvares <fredericoalvares@gmail.com>
2019-03-20 17:30:04 +0100

    block storage

--- com/easyvirt/influxdb/linux/scripts/influxdb_pre_configure_volume.sh
+++ com/easyvirt/influxdb/linux/scripts/influxdb_pre_configure_volume.sh
@@ -1,1 +1,10 @@
 #!/bin/bash -e
+
+echo "-------------------------------------------"
+echo " BLOCK STORAGE CONFIGURATION "
+echo ${TARGET_BLOCKSTORAGE_DEVICE} 
+
+ls ${TARGET_BLOCKSTORAGE_DEVICE}
+
+echo "-------------------------------------------"
+
