67a74d10e6a430d20869f454f4557bfeb4ccc537
Frederico Alvares <fredericoalvares@gmail.com>
2019-03-20 18:26:32 +0100

    Configure Influx with existing volume data

--- com/easyvirt/influxdb/linux/scripts/configure_influxdb.sh
+++ com/easyvirt/influxdb/linux/scripts/configure_influxdb.sh
@@ -5,6 +5,11 @@ sudo systemctl stop influxdb
 DEFAULT_API_PORT=8086 
 DEFAULT_RCP_PORT=8088 
 
+ORIGINAL_PATH="\/var\/lib\/influxdb\/"
+DEFAULT_PATH="\/mountedStorageNb\/influxdb\/"
+
+
+
 CONF_FILE="/etc/influxdb/influxdb.conf"
 
 if [[ ("$API_PORT" == "$DEFAULT_API_PORT") ]]; then
@@ -20,6 +25,17 @@ else
   echo "Replacing RCP port $DEFAULT_RCP_PORT with $RCP_PORT..."
   sudo sed -i -e "s/$DEFAULT_RCP_PORT/$RCP_PORT/g" $CONF_FILE || exit ${1}
 fi
+
+
+echo "Replacing data and meta path..."
+sudo sed -i -e "s/dir = \"${ORIGINAL_PATH}meta\"/dir = \"${DEFAULT_PATH}meta\"/g" $CONF_FILE || exit ${1}
+sudo sed -i -e "s/dir = \"${ORIGINAL_PATH}data\"/dir = \"${DEFAULT_PATH}data\"/g" $CONF_FILE || exit ${1}
+sudo sed -i -e "s/wal-dir = \"${ORIGINAL_PATH}wal\"/wal-dir = \"${DEFAULT_PATH}wal\"/g" $CONF_FILE || exit ${1}
+
+
+
+cat $CONF_FILE
+
 
 echo "Start influxdb whith new conf"
 sudo service influxdb start
